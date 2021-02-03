5730f3786f456ba4e4c9ca4a455d84b4b1e975cb
andres <afquinteromoreano@gmail.com>
2021-02-03 10:25:17 -0500

    ref #62 funciones para leer y aplicar perfil

--- R/funciones_perfil.R
+++ R/funciones_perfil.R
@@ -1,10 +1,37 @@
 leer_perfil <- function(json) {
   perfil <- parse_json(json, simplifyVector = TRUE)
+  return(perfil)
   
-  perfil_transform <- lapply(perfil, as.data.frame) %>%
-    rbindlist(fill = TRUE, idcol = "perfil")
-    
+}
+
+perfil_jerarquia <- function(perfiles, perfil_select, items, 
+                             funcion_jerarquia, ns) {
   
-  return(perfil_transform)
+  perfiles_seleccionado <- perfiles[[perfil_select]][["jerarquia"]]
+  
+  items_nivel_1 <- perfiles_seleccionado[["episodio"]]
+  items_nivel_1 <- items_nivel_1[items_nivel_1 %in% items]
+  
+  items_nivel_2 <- perfiles_seleccionado[["factura"]]
+  items_nivel_2 <- items_nivel_2[items_nivel_2 %in% items]
+  
+  items_nivel_3 <- perfiles_seleccionado[["paciente"]]
+  items_nivel_3 <- items_nivel_3[items_nivel_3 %in% items]
+  
+  items_nivel_4 <- setdiff(
+    x = items,
+    y = c(items_nivel_1, items_nivel_2, items_nivel_3))
+  
+  if (identical(items_nivel_4, character(0))) {
+    items_nivel_4 <- NULL
+  }
+  
+  funcion_jerarquia(
+    ns = ns,
+    items_nivel_1 = items_nivel_1,
+    items_nivel_2 = items_nivel_2,
+    items_nivel_3 = items_nivel_3,
+    items_nivel_4 = items_nivel_4
+  )
   
 }
