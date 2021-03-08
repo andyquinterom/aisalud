8eac4083424e76ec5bcee384c6274be77207ba56
andres <afquinteromoreano@gmail.com>
2021-03-08 17:29:34 -0500

    Ref #103 calculo de las diferentes tablas por valor facturado

--- modules/seguimiento_notas_dashboard.R
+++ modules/seguimiento_notas_dashboard.R
@@ -672,6 +672,7 @@ seguimiento_notas_dashboard_server <- function(id, opciones) {
         if (!is.null(opciones$colnames) && !is.null(input$comparar_agrupador) &&
             input$comparar_agrupador %notin% c("", "Ninguno")) {
           comparar$frecs <- list()
+          comparar$val_fact <- list()
           agrupador <- input$comparar_agrupador
           comparar_col_valor <- input$comparar_col_valor
           tryCatch(
@@ -687,14 +688,43 @@ seguimiento_notas_dashboard_server <- function(id, opciones) {
                   nivel_2 = input$episodios_jerarquia_nivel_2_order,
                   nivel_3 = input$episodios_jerarquia_nivel_3_order,
                   nivel_4 = input$episodios_jerarquia_nivel_4_order)[["descriptiva"]]
+                
+                comparar$datos$valor_factura_tabla <- opciones$tabla %>%
+                  group_by(!!!rlang::syms(comparar_col_valor)) %>%
+                  mutate(
+                    ais_mes  = month(max(fecha_prestacion, na.rm = TRUE)),
+                    ais_anio =  year(max(fecha_prestacion, na.rm = TRUE))) %>%
+                  ungroup() %>%
+                  episodios_jerarquia(
+                    data = .,
+                    columnas = agrupador,
+                    columna_sep = c("ais_anio", "ais_mes"),
+                    columna_valor = opciones$valor_costo,
+                    columna_suma = comparar_col_valor,
+                    nivel_1 = input$episodios_jerarquia_nivel_1_order,
+                    nivel_2 = input$episodios_jerarquia_nivel_2_order,
+                    nivel_3 = input$episodios_jerarquia_nivel_3_order,
+                    nivel_4 = input$episodios_jerarquia_nivel_4_order
+                  )
+                
               } else {
                 comparar$datos$frecuencias_tabla <- frecuencias(
                   columna_fecha = "fecha_prestacion",
                   data = opciones$tabla,
                   agrupador = agrupador,
-                  columna_suma = input$descriptiva_unidades,
+                  columna_suma = comparar_col_valor,
                   prestaciones = (input$descriptiva_unidades == "prestacion")
                 )
+                
+                comparar$datos$valor_factura_tabla <- opciones$tabla %>%
+                  mutate(ais_mes  = month(fecha_prestacion),
+                         ais_anio =  year(fecha_prestacion)) %>%
+                  descriptiva(
+                    columnas = c(agrupador, "ais_anio", "ais_mes"),
+                    columna_suma = comparar_col_valor,
+                    columna_valor = opciones$valor_costo,
+                    prestaciones = (input$descriptiva_unidades == "prestacion")
+                  )
               }
             
               nt_test <- nt_opciones$datos
@@ -1001,5 +1031,163 @@ comparacion_frecuencias <- function(frecuencias_tabla, nota_tecnica, agrupador) 
               valor_acumulado = valor_acumulado,
               plot_valor_acumulado = plot_valor_acumulado
               ))
+  
+}
+
+comparacion_valor_facturado <- function(
+  descriptiva_tabla, nota_tecnica, agrupador, col_mes = "ais_mes",
+  col_anio = "ais_anio") {
+  
+  descriptiva_tabla <- descriptiva_tabla %>%
+    mutate(ais_mes_anio = !!rlang::sym(col_anio) * 100 + !!rlang::sym(col_mes))
+  
+  style_interval <-ifelse(
+    test = (Sys.getenv("NT_MODO_IPS") != "") %>% rep(2),
+    yes = c("rgb(145, 255, 145)", "rgb(255, 145, 145)"),
+    no = c("rgb(255, 145, 145)", "rgb(145, 255, 145)"))
+  
+  comparaciones <- purrr::map(
+    .x = list("suma" = "suma", "diff" = "diff", "perc" = "perc"),
+    .f = comparar_nt_valor_factura,
+    descriptiva_tabla = descriptiva_tabla,
+    nota_tecnica = nota_tecnica,
+    agrupador = agrupador,
+    col_anio = col_anio,
+    col_mes = col_mes
+  )
+  
+  comparacion_suma_dt <- datatable(
+    data = comparaciones[["suma"]],
+    colnames = c(
+      "Valor a mes" = "valor_mes",
+      "Suma valor total" = "total",
+      "Agrupador" = "agrupador"),
+    rownames = FALSE,
+    extensions = c('FixedColumns'),
+    selection = 'none',
+    options = list(
+      dom = 't',
+      pageLength = nrow(comparaciones[["suma"]]),
+      scrollX = TRUE,
+      fixedColumns = list(leftColumns = 2, rightColumns = 1),
+      language = list(
+        url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Spanish.json'))) %>%
+    formatCurrency(
+      columns = 2:ncol(comparaciones[["suma"]]),
+      dec.mark = ",", mark = ".", digits = 0)
+  
+  comparacion_diff_dt <- datatable(
+    data = comparaciones[["diff"]],
+    colnames = c(
+      "Valor a mes" = "valor_mes",
+      "Diferencia de valor total" = "total",
+      "Agrupador" = "agrupador"),
+    rownames = FALSE,
+    extensions = c('FixedColumns'),
+    selection = 'none',
+    options = list(
+      dom = 't',
+      pageLength = nrow(comparaciones[["diff"]]),
+      scrollX = TRUE,
+      fixedColumns = list(leftColumns = 2, rightColumns = 1),
+      language = list(
+        url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Spanish.json'))) %>%
+    formatCurrency(
+      columns = 2:ncol(comparaciones[["diff"]]),
+      dec.mark = ",", mark = ".", digits = 0) %>%
+    formatStyle(
+      columns = c(3:ncol(comparaciones[["diff"]]),
+                  ncol(comparaciones[["diff"]])),
+      backgroundColor = styleInterval(
+        cuts = 0,
+        values = style_interval
+      ))
+  
+  comparacion_perc_dt <- datatable(
+    data = comparaciones[["perc"]],
+    colnames = c(
+      "Valor a mes" = "valor_mes",
+      "Porcentaje de ejecución medio" = "media",
+      "Ejecución media a mes" = "media_valor",
+      "Agrupador" = "agrupador"),
+    rownames = FALSE,
+    extensions = c('FixedColumns'),
+    selection = 'none',
+    options = list(
+      dom = 't',
+      pageLength = nrow(comparaciones[["perc"]]),
+      scrollX = TRUE,
+      fixedColumns = list(leftColumns = 2, rightColumns = 2),
+      language = list(
+        url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Spanish.json'))) %>%
+    formatPercentage(
+      columns = 3:(ncol(comparaciones[["perc"]]) - 1),
+      dec.mark = ",", mark = ".", digits = 0) %>%
+    formatStyle(
+      columns = 3:(ncol(comparaciones[["perc"]]) - 1),
+      backgroundColor = styleInterval(
+        cuts = 1,
+        values = style_interval
+      )) %>%
+    formatCurrency(
+      columns = c(2, ncol(comparaciones[["perc"]])),
+      dec.mark = ",", mark = ".", digits = 0)
+  
+  minimo_mes <- min(pull(descriptiva_tabla, ais_mes_anio))
+  maximo_mes <- max(pull(descriptiva_tabla, ais_mes_anio))
+  numero_meses <- maximo_mes - minimo_mes + 1
+  valor_mes <- sum(nota_tecnica$valor_mes)
+   
+  print(numero_meses)
+  
+  meses_completos <- tibble("ais_mes_anio" = seq(minimo_mes, maximo_mes)) %>%
+    mutate(ais_anio = substr(ais_mes_anio, 1, 4) %>% as.numeric(),
+           ais_mes  = substr(ais_mes_anio, 5, 6) %>% as.numeric()) %>%
+    filter(ais_mes >= 1 & ais_mes <= 12) %>%
+    select(-ais_mes_anio)
+  
+  valor_acumulado <- meses_completos %>%
+    left_join(
+      descriptiva_tabla %>%
+        rename(agrupador = !!as.name(agrupador)) %>%
+        left_join(nota_tecnica %>%
+                     select(agrupador)) %>%
+        arrange(!!rlang::sym(col_anio), !!rlang::sym(col_mes)) %>%
+        group_by(!!rlang::sym(col_anio), !!rlang::sym(col_mes)) %>%
+        summarise(Suma = sum(Suma, na.rm = TRUE)) %>%
+        select(!!!rlang::syms(c(col_anio, col_mes)), Suma),
+      by = c("ais_mes" = col_mes, "ais_anio" = col_anio)) %>%
+    mutate(numero_meses = 1:nrow(.), Suma = replace_na(Suma, 0),
+           valor_acumulado = cumsum(Suma), valor_mes_esperado = valor_mes,
+           valor_a_ejecutar = valor_mes * as.double(numero_meses),
+           mes_anio_num = ais_anio * 100 + ais_mes,
+           mes_anio = mes_spanish_juntos(mes_anio_num))
+  
+  plot_valor_acumulado <- valor_acumulado %>%
+    plot_ly(
+      x = ~mes_anio_num,
+      y = ~valor_acumulado, 
+      name = "Valor ejecutado",
+      type = "scatter", mode = "lines+markers"
+    ) %>%
+    add_trace(
+      y = ~valor_a_ejecutar,
+      name = "Valor a ejecutar",
+      mode = "lines",
+      line = list(color = 'rgb(205, 12, 24)', dash = 'dash'),
+      fill = 'tonexty', fillcolor='rgba(0,100,80,0.2)'
+    ) %>%
+    config(locale = "es") %>%
+    layout(
+      legend = list(x = 0.1, y = 0.9),
+      xaxis = list(
+        title = "Mes",
+        ticktext = as.list(valor_acumulado$mes_anio),
+        tickvals = as.list(valor_acumulado$mes_anio_num),
+        tickmode = "array"
+      ),
+      yaxis = list(title = "Suma",
+                   tickformat = ",.2f")
+    ) 
   
 }
