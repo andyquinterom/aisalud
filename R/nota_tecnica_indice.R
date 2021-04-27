c0f954983194d01242c76212109c08a91cbed273
andres <afquinteromoreano@gmail.com>
2021-04-27 15:33:00 -0500

    Ref #142 se resuelven conflictos entre version

--- R/nota_tecnica_indice.R
+++ R/nota_tecnica_indice.R
@@ -85,6 +85,7 @@ parse_nt <- function(x) {
     agrupadores_names <- names(y[["agrupadores"]])
 
     purrr::map2(y[["agrupadores"]], agrupadores_names, function(i, w) {
+      if (is.null(names(i))) i <- list("n" = i[1], "cm" = i[2])
       temp <- data.frame("agrupador" = w, "frec_mes" = as.double(i[["n"]]),
                          "cm" = as.double(i[["cm"]]), 
                          "frecuencia_pc" = as.double(i[["n"]]/poblacion),
