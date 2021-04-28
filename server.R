3710ac1fb69eab6a92c277872f0ceb6786a2d3f4
andres <afquinteromoreano@gmail.com>
2021-04-28 10:09:56 -0500

    Ref #151 se manifiesta flow de datos en server.R

--- server.R
+++ server.R
@@ -1,3 +1,25 @@
+# El backend de Analítica Integrada Salud seguira una estructura modular.
+# El flow de datos empieza en el módulo carga_datos y alimentan a los otros.
+# La información compartida se manifiesta en la variable opciones.
+# ┌────────────┐   ┌─────┐
+# │            │◄──┤ pSQL│
+# │            │   ├─────┤
+# │cargar_datos│◄──┤ UI  │
+# │            │   ├─────┤
+# │            │◄──┤ WEB │
+# └─────┬──────┘   └─────┘
+#       │
+#       │    ┌───────────┐
+#       │◄──►│  Filtros  │
+#       │    └───────────┘
+#       ▼
+# ┌──────────────────────┐
+# │        Módulos       │
+# └──────────────────────┘
+#
+# Los módulos deben ser self containing. No deben afectar el comportamiento
+# de otros módulos. Solamente el módulo de filtros podrá cambiar datos.
+
 shinyServer(function(input, output, session) {
   
   opciones <- reactiveValues(
