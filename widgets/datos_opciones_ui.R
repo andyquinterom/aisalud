0a1ac9b922df5d5c9814d319d4ffbedfb881e52b
andres <afquinteromoreano@gmail.com>
2021-05-28 09:51:20 -0500

    Ref #151 mejoras de estilo de código

--- widgets/datos_opciones_ui.R
+++ widgets/datos_opciones_ui.R
@@ -14,25 +14,3 @@ datos_opciones_ui <- function(
     )
   }
 }
-
-# Opciones para archivos csv
-datos_opciones_csv_ui <- function(id, value_delimitador, value_decimal) {
-  ns <- NS(id)
-  tagList(
-    radioButtons(
-      inputId = ns("value_delimitador"),
-      choices = c(",", ";", "|", "Espacios"),
-      label = "Delimitador",
-      inline = TRUE,
-      selected = value_delimitador
-    ),
-    radioButtons(
-      inputId = ns("value_decimal"),
-      choiceNames = c("Punto", "Coma"),
-      choiceValues = c(".", ","),
-      label = "Separador decimal",
-      inline = TRUE,
-      selected = value_decimal
-    )
-  )
-}
