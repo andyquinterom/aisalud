e21218e5d8dd114022d7958e708ae25c682c3830
andres <afquinteromoreano@gmail.com>
2021-05-28 08:28:20 -0500

    Ref #151 mejoras de estilo de código

--- R/dbListNumericFields.R
+++ R/dbListNumericFields.R
@@ -9,7 +9,7 @@ from information_schema.columns col
 join information_schema.tables tab on tab.table_schema = col.table_schema
                                    and tab.table_name = col.table_name
                                    and tab.table_type = 'BASE TABLE'
-where col.data_type in ('smallint', 'integer', 'bigint', 
+where col.data_type in ('smallint', 'integer', 'bigint',
                         'decimal', 'numeric', 'real', 'double precision',
                         'smallserial', 'serial', 'bigserial', 'money')
       and col.table_schema not in ('information_schema', 'pg_catalog')
