e4def44ef57118aeffb52e9b5fc62a8f57b03762
andres <afquinteromoreano@gmail.com>
2021-05-28 08:35:18 -0500

    Ref #151 mejoras de estilo de código

---
+++ R/formatAsPerc.R
@@ -0,0 +1,13 @@
+formatAsPerc <- function(x) {
+  x <- as.numeric(as.character(x))
+  return(
+    paste0(
+      format(
+        x,
+        digits = 0,
+        big.mark = ".",
+        decimal.mark = ",",
+        scientific = F),
+      "%")
+  )
+}
