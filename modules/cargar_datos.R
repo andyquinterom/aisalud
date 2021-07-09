bb7a433d5c2b4d5c07374f968d41b35c7ccf3b83
andres <afquinteromoreano@gmail.com>
2021-07-09 10:59:05 -0500

    Ref #184 se agrega header de licencia a todos los archivos en las carpetas R y modules
    Signed-off-by: andres <afquinteromoreano@gmail.com>

--- modules/cargar_datos.R
+++ modules/cargar_datos.R
@@ -1,3 +1,18 @@
+#
+# Analítica Integrada Salud
+#
+# Derechos de autor 2021 por MD&CO Consulting Group (NIT 901.119.781-5)
+# Copyright (C) 2021 by MD&CO Consulting Group
+#
+# Este programa es software libre: puede redistribuirlo o modificarlo bajo
+# los términos de la licencia Affero General Public License tal cual
+# publicada por la Free Software Foundation, sea la versión 3 de la licencia
+# o cualquier versión posterior. Este programa se distribuye SIN GARANTÍA
+# EXPERSA O IMPLÍCITA, INCLUIDAS LAS DE NO INFRACCIÓN, COMERCIABILIDAD O
+# APTITUD PARA UN PROPÓSITO PARTICULAR. Referir a la
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) para más detalles.
+#
+
 # El módulo de cargar_datos tiene como proposito alimentar los datos de
 # toda la aplicación. La varaible principal en la cual se almacenan los datos
 # es la opciones. Esta se declara en el server.R y se muta dentro de este
